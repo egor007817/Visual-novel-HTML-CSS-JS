7d011459ad94eb972e87a2ec8781ca99b3fd6d81
Егор <138401994+EgorNeVor@users.noreply.github.com>
2023-11-17 20:27:25 +0300

    Add files via upload

---
+++ visual novel sd/js.js
@@ -0,0 +1,389 @@
+function onb(){
+    $('#div12').hide()
+    $('#c1').hide()
+    $('#c2').show()
+    $('#dib').show()
+}
+function onb1(){
+    $('#c2').hide()
+    $('#dib').hide()
+    $('#c3').show()
+    $('#dib1').show()
+}
+function onb2(){
+    $('#c3').hide()
+    $('#dib1').hide()
+    $('#c4').show()
+    $('#dib2').show()
+}
+function onb3(){
+    $('#c4').hide()
+    $('#dib2').hide()
+    $('#c5').show()
+    $('#dib3').show()
+}
+function onb4(){
+    $('#c5').hide()
+    $('#dib3').hide()
+    $('#onef').hide()
+    $('#onef1').show()
+    $('#c6').show()
+    $('#dib4').show()
+}
+function onb5(){
+    $('#c6').hide()
+    $('#dib4').hide()
+    $('#c7').show()
+    $('#dib5').show()
+}
+function onb6(){
+    $('#c7').hide()
+    $('#dib5').hide()
+    $('#c8').show()
+    $('#dib6').show()
+}
+function onb7(){
+    $('#c8').hide()
+    $('#dib6').hide()
+    $('#onef1').hide()
+    $('#onef2').show()
+    $('#c9').show()
+    $('#dib7').show()
+}
+function onb8(){
+    $('#c9').hide()
+    $('#dib7').hide()
+    $('#c10').show()
+    $('#dib8').show()
+}
+function onb9(){
+    $('#c10').hide()
+    $('#dib8').hide()
+    $('#c11').show()
+    $('#dib9').show()
+}
+function onb10(){
+    $('#c11').hide()
+    $('#dib9').hide()
+    $('#c12').show()
+    $('#dib10').show()
+}
+function onb11(){
+    $('#c12').hide()
+    $('#dib10').hide()
+    $('#onef2').hide()
+    $('#onef3').show()
+    $('#c13').show()
+    $('#dib11').show()
+}
+function onb12(){
+    $('#c13').hide()
+    $('#dib11').hide()
+    $('#c14').show()
+    $('#dib12').show()
+}
+function onb13(){
+    $('#c14').hide()
+    $('#dib12').hide()
+    $('#c15').show()
+    $('#dib13').show()
+}
+function onb14(){
+    $('#c15').hide()
+    $('#dib13').hide()
+    $('#onef3').hide()
+    $('#onef4').show()
+    $('#c16').show()
+    $('#dib14').show()
+}
+function onb15(){
+    $('#c16').hide()
+    $('#dib14').hide()
+    $('#c17').show()
+    $('#dib15').show()
+}
+function onb16(){
+    $('#c17').hide()
+    $('#dib15').hide()
+    $('#onef4').hide()
+    $('#onef5').show()
+    $('#c18').show()
+    $('#dib16').show()
+}
+function onb17(){
+    $('#c18').hide()
+    $('#dib16').hide()
+    $('#c19').show()
+    $('#dib17').show()
+}
+function onb18(){
+    $('#c19').hide()
+    $('#dib17').hide()
+    $('#c20').show()
+    $('#dib18').show()
+}
+function onb19(){
+    $('#c20').hide()
+    $('#dib18').hide()
+    $('#onef5').hide()
+    $('#onef6').show()
+    $('#c21').show()
+    $('#dib19').show()
+}
+function onb20(){
+    $('#c21').hide()
+    $('#dib19').hide()
+    $('#c22').show()
+    $('#dib20').show()
+}
+function onb21(){
+    $('#c22').hide()
+    $('#dib20').hide()
+    $('#onef6').hide()
+    $('#onef7').show()
+    $('#c23').show()
+    $('#dib21').show()
+}
+function onb22(){
+    $('#c23').hide()
+    $('#dib21').hide()
+    $('#c24').show()
+    $('#dib22').show()
+}
+function onb23(){
+    $('#c24').hide()
+    $('#dib22').hide()
+    $('#c25').show()
+    $('#dib23').show()
+}
+function onb24(){
+    $('#c25').hide()
+    $('#dib23').hide()
+    $('#onef7').hide()
+    $('#onef8').show()
+    $('#c26').show()
+    $('#dib24').show()
+}
+function onb25(){
+    $('#c26').hide()
+    $('#dib24').hide()
+    $('#c27').show()
+    $('#dib25').show()
+}
+function onb26(){
+    $('#c27').hide()
+    $('#dib25').hide()
+    $('#onef8').hide()
+    $('#onef7').show()
+    $('#c28').show()
+    $('#dib26').show()
+}
+function onb27(){
+    $('#c28').hide()
+    $('#dib26').hide()
+    $('#c29').show()
+    $('#dib27').show()
+}
+function onb28(){
+    $('#c29').hide()
+    $('#dib27').hide()
+    $('#onef7').hide()
+    $('#onef8').show()
+    $('#c30').show()
+    $('#dib28').show()
+}
+function onb29(){
+    $('#c30').hide()
+    $('#dib28').hide()
+    $('#onef8').hide()
+    $('#onef4').show()
+    $('#c31').show()
+    $('#dib29').show()
+}
+function onb30(){
+    $('#c31').hide()
+    $('#dib29').hide()
+    $('#c32').show()
+    $('#dib30').show()
+}
+function onb31(){
+    $('#c32').hide()
+    $('#dib30').hide()
+    $('#c33').show()
+    $('#dib31').show()
+}
+function onb32(){
+    $('#c33').hide()
+    $('#dib31').hide()
+    $('#onef4').hide()
+    $('#onef3').show()
+    $('#c34').show()
+    $('#dib32').show()
+}
+function onb33(){
+    $('#c34').hide()
+    $('#dib32').hide()
+    $('#c35').show()
+    $('#dib33').show()
+}
+function onb34(){
+    $('#c35').hide()
+    $('#dib33').hide()
+    $('#onef3').hide()
+    $('#onef11').show()
+    $('#c36').show()
+    $('#dib34').show()
+
+}
+function onb35(){
+    $('#c36').hide()
+    $('#dib34').hide()
+    $('#onef11').hide()
+    $('#onef12').show()
+    $('#c37').show()
+    $('#dib35').show()
+}
+function onb36(){
+    $('#c37').hide()
+    $('#dib34').hide()
+    $('#c38').show()
+    $('#dib36').show()
+}
+function onb37(){
+    $('#c38').hide()
+    $('#dib36').hide()
+    $('#onef12').hide()
+    $('#onef13').show()
+    $('#c39').show()
+    $('#dib37').show()
+}
+function onb38(){
+    $('#c39').hide()
+    $('#dib37').hide()
+    $('#c40').show()
+    $('#dib38').show()
+}
+function onb39(){
+    $('#c40').hide()
+    $('#dib38').hide()
+    $('#c41').show()
+    $('#dib39').show()
+}
+function onb40(){
+    $('#c41').hide()
+    $('#dib39').hide()
+    $('#onef13').hide()
+    $('#onef14').show()
+    $('#c42').show()
+    $('#dib40').show()
+}
+function onb41(){
+    $('#c42').hide()
+    $('#dib40').hide()
+    $('#c43').show()
+    $('#dib41').show()
+}
+function onb42(){
+    $('#c43').hide()
+    $('#dib41').hide()
+    $('#c44').show()
+    $('#dib42').show()
+}
+function onb43(){
+    $('#c44').hide()
+    $('#dib42').hide()
+    $('#onef14').hide()
+    $('#onef15').show()
+    $('#c45').show()
+    $('#dib43').show()
+}
+function onb44(){
+    $('#c45').hide()
+    $('#dib43').hide()
+    $('#c46').show()
+    $('#dib44').show()
+}
+function onb45(){
+    $('#c46').hide()
+    $('#dib44').hide()
+    $('#c47').show()
+    $('#dib45').show()
+}
+function onb46(){
+    $('#c47').hide()
+    $('#dib45').hide()
+    $('#c48').show()
+    $('#dib46').show()
+}
+function onb47(){
+    $('#c48').hide()
+    $('#dib46').hide()
+    $('#onef15').hide()
+    $('#onef14').show()
+    $('#c49').show()
+    $('#dib47').show()
+}
+function onb48(){
+    $('#c49').hide()
+    $('#dib47').hide()
+    $('#c50').show()
+    $('#dib48').show()
+}
+function onb49(){
+    $('#c50').hide()
+    $('#dib48').hide()
+    $('#v1').show()
+    $('#v2').show()
+}
+function onb50(){
+    $('#v1').hide()
+    $('#v2').hide()
+    $('#c51').show()
+    $('#dib49').show()
+}
+function onb51(){
+    $('#c51').hide()
+    $('#dib49').hide()
+    $('#c52').show()
+    $('#dib50').show()
+}
+function onb52(){
+    $('#c52').hide()
+    $('#dib50').hide()
+    $('#onef14').hide()
+    $('#onef11').show()
+    $('#c53').show()
+    $('#dib51').show()
+}
+function onb53(){
+    $('#c53').hide()
+    $('#dib51').hide()
+    $('#c54').show()
+    $('#dib52').show()
+}
+function onb54(){
+    $('#c54').hide()
+    $('#dib52').hide()
+    $('#c55').show()
+    $('#dib53').show()
+}
+function onb55(){
+    $('#c55').hide()
+    $('#dib53').hide()
+    $('#onef11').hide()
+    $('#onef').show()
+    $('#c56').show()
+    $('#dib54').show()
+}
+function onb56(){
+    $('#c56').hide()
+    $('#dib54').hide()
+    $('#c57').show()
+    $('#dib55').show()
+}
+function onb57(){
+    $('#c57').hide()
+    $('#dib55').hide()
+    $('#c58').show()
+    $('#dib56').show()
+}
---
+++ visual novel sd/saljs.js
@@ -0,0 +1,222 @@
+function onbu(){
+    $('#d1').hide()
+    $("#i1").hide()
+    $('#onef14').hide()
+    $('#onef15').show()
+    $('#d2').show()
+    $("#i2").show()
+}
+function onbu1(){
+    $('#d2').hide()
+    $("#i2").hide()
+    $('#d3').show()
+    $("#i3").show()
+}
+function onbu2(){
+    $('#d3').hide()
+    $("#i3").hide()
+    $('#onef15').hide()
+    $('#onef16').show()
+    $('#d4').show()
+    $("#i4").show()
+}
+function onbu3(){
+    $('#d4').hide()
+    $("#i4").hide()
+    $('#d5').show()
+    $("#i5").show()
+}
+function onbu4(){
+    $('#d5').hide()
+    $("#i5").hide()
+    $('#onef16').hide()
+    $('#onef17').show()
+    $('#d6').show()
+    $("#i6").show()
+}
+function onbu5(){
+    $('#d6').hide()
+    $("#i6").hide()
+    $('#d7').show()
+    $("#i7").show()
+}
+function onbu6(){
+    $('#d7').hide()
+    $("#i7").hide()
+    $('#d8').show()
+    $("#i8").show()
+}
+function onbu7(){
+    $('#d8').hide()
+    $("#i8").hide()
+    $('#d9').show()
+    $("#i9").show()
+}
+function onbu8(){
+    $('#d9').hide()
+    $("#i9").hide()
+    $('#d10').show()
+    $("#i10").show()
+}
+function onbu9(){
+    $('#d10').hide()
+    $("#i10").hide()
+    $('#onef17').hide()
+    $('#onef18').show()
+    $('#d11').show()
+    $("#i11").show()
+}
+function onbu10(){
+    $('#d11').hide()
+    $("#i11").hide()
+    $('#d12').show()
+    $("#i12").show()
+}
+function onbu11(){
+    $('#d12').hide()
+    $("#i12").hide()
+    $('#d13').show()
+    $("#i13").show()
+}
+function onbu12(){
+    $('#d13').hide()
+    $("#i13").hide()
+    $('#d14').show()
+    $("#i14").show()
+}
+function onbu13(){
+    $('#d14').hide()
+    $("#i14").hide()
+    $('#onef18').hide()
+    $('#onef17').show()
+    $('#d15').show()
+    $("#i15").show()
+}
+function onbu14(){
+    $('#d15').hide()
+    $("#i15").hide()
+    $('#d16').show()
+    $("#i16").show()
+}
+function onbu15(){
+    $('#d16').hide()
+    $("#i16").hide()
+    $('#d17').show()
+    $("#i17").show()
+}
+function onbu16(){
+    $('#d17').hide()
+    $("#i17").hide()
+    $('#d18').show()
+    $("#i18").show()
+}
+function onbu17(){
+    $('#d18').hide()
+    $("#i18").hide()
+    $('#d19').show()
+    $("#i19").show()
+}
+function onbu18(){
+    $('#d19').hide()
+    $("#i19").hide()
+    $('#d20').show()
+    $("#i20").show()
+}
+function onbu19(){
+    $('#d20').hide()
+    $("#i20").hide()
+    $('#onef17').hide()
+    $('#onef18').show()
+    $('#d21').show()
+    $("#i21").show()
+}
+function onbu20(){
+    $('#d21').hide()
+    $("#i21").hide()
+    $('#d22').show()
+    $("#i22").show()
+}
+function onbu21(){
+    $('#d22').hide()
+    $("#i22").hide()
+    $('#d23').show()
+    $("#i23").show()
+}
+function onbu22(){
+    $('#d23').hide()
+    $("#i23").hide()
+    $('#d24').show()
+    $("#i24").show()
+}
+function onbu23(){
+    $('#d24').hide()
+    $("#i24").hide()
+    $('#onef18').hide()
+    $('#onef17').show()
+    $('#d25').show()
+    $("#i25").show()
+}
+function onbu24(){
+    $('#d25').hide()
+    $("#i25").hide()
+    $('#d26').show()
+    $("#i26").show()
+}
+function onbu25(){
+    $('#d26').hide()
+    $("#i26").hide()
+    $('#d27').show()
+    $("#i27").show()
+}
+function onbu26(){
+    $('#d27').hide()
+    $("#i27").hide()
+    $('#d28').show()
+    $("#i28").show()
+}
+function onbu27(){
+    $('#d28').hide()
+    $("#i28").hide()
+    $('#onef17').hide()
+    $('#onef19').show()
+    $('#d29').show()
+    $("#i29").show()
+}
+function onbu28(){
+    $('#d29').hide()
+    $("#i29").hide()
+    $('#d30').show()
+    $("#i30").show()
+}
+function onbu29(){
+    $('#d30').hide()
+    $("#i30").hide()
+    $('#onef19').hide()
+    $('#onef').show()
+    $('#d31').show()
+    $("#i31").show()
+}
+function onbu30(){
+    $('#d31').hide()
+    $("#i31").hide()
+    $('#d32').show()
+    $("#i32").show()
+}
+function onbu31(){
+    $('#d32').hide()
+    $("#i32").hide()
+    $('#d33').show()
+    $("#i33").show()
+}
+function onbu32(){
+    $('#d33').hide()
+    $("#i33").hide()
+    $('#d34').show()
+    $("#i34").show()
+}
+function onbu33(){
+    $('#d34').hide()
+    $("#i34").hide()
+    $('#d35').show()
+    $("#i35").show()
+}
